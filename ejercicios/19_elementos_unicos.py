9c12feb76d6c4719548c3fa05107cae89b419d22
Jose Ojeda <josecodetech@gmail.com>
2023-11-27 07:24:10 +0100

    ejercicios

---
+++ ejercicios/19_elementos_unicos.py
@@ -0,0 +1,4 @@
+import numpy as np
+array = np.array([1, 2, 2, 3, 4, 4, 5])
+valores_unicos = np.unique(array)
+print(valores_unicos)
---
+++ ejercicios/20_raiz_cuadrada_array.py
@@ -0,0 +1,4 @@
+import numpy as np
+array = np.array([1, 4, 9, 16, 25])
+sqrt_array = np.sqrt(array)
+print(sqrt_array)
